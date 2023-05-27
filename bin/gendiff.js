0b05c039d5a33db52060f52cabfeb5d2eb2a7fc2
Olga Efremova <pcholyk@yandex.ru>
2023-05-27 22:00:03 +0300

    add filepath arguments and output format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,10 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Tool to work with files')
-  .version('0.0.1');
+  .version('0.0.1')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format');
 
 program
   .description('Compares two configuration files and shows a difference.')
