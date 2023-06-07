c7970d9f33ff0bce6cd5608a44300edcd553af72
Olga Efremova <pcholyk@yandex.ru>
2023-06-07 22:52:54 +0300

    add function in action, update description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
   .name('gendiff')
-  .description('Tool to work with files')
+  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format');
-
-program
-  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
   .helpOption('-h, --help', 'output usage information');
 
 program.parse();
