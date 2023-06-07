c4665b9a1b978e38b7480d8bafc6b66fd853c96c
Olga Efremova <pcholyk@yandex.ru>
2023-06-07 22:56:32 +0300

    make gendiff function

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// import fs from 'fs';
+import { cwd } from 'node:process';
+import path from 'path';
+import _ from 'lodash';
+import getFileContentAsObject from './parse.js';
+
+// const getFileContentAsObject = (filePath) => {
+//  const content = fs.readFileSync(filePath, { encoding: 'utf8' });
+//  return JSON.parse(content);
+// };
+
+const getAbsolutePath = (givenPath) => path.resolve(cwd(), givenPath);
+
+const compareProperties = (object1, object2, propertie) => {
+  if (Object.hasOwn(object1, propertie) && Object.hasOwn(object2, propertie)) {
+    if (object1[propertie] === object2[propertie]) {
+      return `   ${propertie}: ${object1[propertie]}`;
+    }
+    return ` - ${propertie}: ${object1[propertie]}\n  + ${propertie}: ${object2[propertie]}`;
+  }
+  if (!Object.hasOwn(object1, propertie) && Object.hasOwn(object2, propertie)) {
+    return ` + ${propertie}: ${object2[propertie]}`;
+  }
+  return ` - ${propertie}: ${object1[propertie]}`;
+};
+
+const genDiff = (path1, path2) => {
+  const contentOfFile1 = getFileContentAsObject(getAbsolutePath(path1));
+  const contentOfFile2 = getFileContentAsObject(getAbsolutePath(path2));
+  const combinedObj = { ...contentOfFile1, ...contentOfFile2 };
+  const keys = _.orderBy(Object.keys(combinedObj));
+  const result = keys.reduce((acc, key) => `${acc}\n ${compareProperties(contentOfFile1, contentOfFile2, key)}`, '');
+  return `{${result}\n}`;
+};
+
+export default genDiff;
