380ae11c8259ccefb705600549510cfeb8208478
Olga Efremova <pcholyk@yandex.ru>
2023-06-20 21:52:12 +0300

    add tests for yaml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,8 +12,12 @@ const expectedResult = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 12
 describe('path', () => {
   test('relative path', () => {
     expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectedResult);
+    expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(expectedResult);
+    expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(expectedResult);
   });
   test('absolute path', () => {
     expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expectedResult);
+    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(expectedResult);
+    expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toEqual(expectedResult);
   });
 });
