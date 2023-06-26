7e2514b7b4f9607839113250acb7dd6855c95917
Olga Efremova <pcholyk@yandex.ru>
2023-06-26 18:50:47 +0300

    delete comments

--- src/parse.js
+++ src/parse.js
@@ -3,8 +3,6 @@
 import yaml from 'js-yaml';
 
 const parse = (content, extension) => {
-//  const extension = path.extname(filePath.toString()).toLowerCase();
-//  const content = fs.readFileSync(filePath, { encoding: 'utf8' });
   if (extension === '.json') {
     return JSON.parse(content);
   } if (extension === '.yml' || extension === '.yaml') {
