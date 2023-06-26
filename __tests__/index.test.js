e99a7b069190771438e320890fd24aa3bc9fdde4
Olga Efremova <pcholyk@yandex.ru>
2023-06-26 18:50:21 +0300

    add fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,17 +7,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expectedResult = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+const expectedResult = '{\n'
++ '  - follow: false\n'
++ '    host: hexlet.io\n'
++ '  - proxy: 123.234.53.22\n'
++ '  - timeout: 50\n'
++ '  + timeout: 20\n'
++ '  + verbose: true\n'
++ '}';
 
 describe('path', () => {
   test('relative path', () => {
-    expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectedResult);
-    expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml')).toEqual(expectedResult);
-    expect(genDiff('__fixtures__/file1.yaml', '__fixtures__/file2.yaml')).toEqual(expectedResult);
+    expect(genDiff('__fixtures__/file1_flat.json', '__fixtures__/file2_flat.json')).toEqual(expectedResult);
+    expect(genDiff('__fixtures__/file1_flat.yml', '__fixtures__/file2_flat.yml')).toEqual(expectedResult);
+    expect(genDiff('__fixtures__/file1_flat.yaml', '__fixtures__/file2_flat.yaml')).toEqual(expectedResult);
   });
   test('absolute path', () => {
-    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expectedResult);
-    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(expectedResult);
-    expect(genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toEqual(expectedResult);
+    expect(genDiff(getFixturePath('file1_flat.json'), getFixturePath('file2_flat.json'))).toEqual(expectedResult);
+    expect(genDiff(getFixturePath('file1_flat.yml'), getFixturePath('file2_flat.yml'))).toEqual(expectedResult);
+    expect(genDiff(getFixturePath('file1_flat.yaml'), getFixturePath('file2_flat.yaml'))).toEqual(expectedResult);
   });
 });
