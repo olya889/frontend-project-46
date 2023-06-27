98bffa41283e1000a80352365750ec14ce1ea8a9
Olga Efremova <pcholyk@yandex.ru>
2023-06-27 21:42:25 +0300

    update test for nested files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,30 +1,14 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-
 import genDiff from '../src/index.js';
+import expectedResult from '../__fixtures__/expected_result.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expectedResult = '{\n'
-+ '  - follow: false\n'
-+ '    host: hexlet.io\n'
-+ '  - proxy: 123.234.53.22\n'
-+ '  - timeout: 50\n'
-+ '  + timeout: 20\n'
-+ '  + verbose: true\n'
-+ '}';
-
-describe('path', () => {
-  test('relative path', () => {
-    expect(genDiff('__fixtures__/file1_flat.json', '__fixtures__/file2_flat.json')).toEqual(expectedResult);
-    expect(genDiff('__fixtures__/file1_flat.yml', '__fixtures__/file2_flat.yml')).toEqual(expectedResult);
-    expect(genDiff('__fixtures__/file1_flat.yaml', '__fixtures__/file2_flat.yaml')).toEqual(expectedResult);
-  });
-  test('absolute path', () => {
-    expect(genDiff(getFixturePath('file1_flat.json'), getFixturePath('file2_flat.json'))).toEqual(expectedResult);
-    expect(genDiff(getFixturePath('file1_flat.yml'), getFixturePath('file2_flat.yml'))).toEqual(expectedResult);
-    expect(genDiff(getFixturePath('file1_flat.yaml'), getFixturePath('file2_flat.yaml'))).toEqual(expectedResult);
-  });
+test('absolute path', () => {
+  expect(genDiff(getFixturePath('before.json'), getFixturePath('after.json'))).toEqual(expectedResult);
+  expect(genDiff(getFixturePath('before.yml'), getFixturePath('after.yml'))).toEqual(expectedResult);
+  expect(genDiff(getFixturePath('before.yaml'), getFixturePath('after.yaml'))).toEqual(expectedResult);
 });
