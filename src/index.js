a46a64dba5c58347fc84925dfa472736a71bc1ac
Olga Efremova <pcholyk@yandex.ru>
2023-07-09 01:13:11 +0300

    update gendiff function for nested files

--- src/index.js
+++ src/index.js
@@ -15,26 +15,82 @@ const getContentAsObject = (filePath) => {
   return parse(content, extension);
 };
 
-const compareProperties = (objectBefore, objectAfter, propertie) => {
-  if (Object.hasOwn(objectBefore, propertie) && Object.hasOwn(objectAfter, propertie)) {
-    if (objectBefore[propertie] === objectAfter[propertie]) {
-      return `   ${propertie}: ${objectBefore[propertie]}`;
+const makeDiffStructure = (objectBefore, objectAfter) => {
+  const combinedObject = { ...objectBefore, ...objectAfter };
+  const keys = _.orderBy(Object.keys(combinedObject));
+  const diffStructure = keys.map((key) => {
+    if (!Object.hasOwn(objectBefore, key)) {
+      return {
+        key, value: objectAfter[key], status: 'added', children: [],
+      };
     }
-    return ` - ${propertie}: ${objectBefore[propertie]}\n  + ${propertie}: ${objectAfter[propertie]}`;
-  }
-  if (!Object.hasOwn(objectBefore, propertie) && Object.hasOwn(objectAfter, propertie)) {
-    return ` + ${propertie}: ${objectAfter[propertie]}`;
-  }
-  return ` - ${propertie}: ${objectBefore[propertie]}`;
+    if (!Object.hasOwn(objectAfter, key)) {
+      return {
+        key, value: objectBefore[key], status: 'deleted', children: [],
+      };
+    }
+    if (objectBefore[key] === objectAfter[key]) {
+      return {
+        key, value: objectBefore[key], status: 'unmodified', children: [],
+      };
+    }
+    if (typeof objectBefore[key] === 'object' && objectBefore[key] !== null && typeof objectAfter[key] === 'object' && objectAfter[key] !== null) {
+      return { key, status: 'nested', children: makeDiffStructure(objectBefore[key], objectAfter[key]) };
+    }
+    return [{
+      key, value: objectBefore[key], status: 'deleted', children: [],
+    },
+    {
+      key, value: objectAfter[key], status: 'added', children: [],
+    },
+    ];
+  });
+  return diffStructure;
+};
+
+const stylish = (structureOfDiff) => {
+  const makeDataAsString = (data, depth) => {
+    if (typeof data === 'object' && data !== null) {
+      const entries = Object.entries(data);
+      const currentIndent = ' '.repeat(depth * 4);
+      const bracketIndent = ' '.repeat((depth - 1) * 4);
+      const stringifiedData = entries.map(([key, value]) => `${currentIndent}${key}:${makeDataAsString(value, depth + 1)}`, '');
+      return [' {', ...stringifiedData, `${bracketIndent}}`].join('\n');
+    }
+    return ` ${(data)}`;
+  };
+  const iter = (diffStructure, currentDepth) => {
+    const bracketInd = ' '.repeat(currentDepth * 4);
+    const leftShift = ' '.repeat(currentDepth * 4 - 2);
+    const formattedResult = diffStructure.reduce((acc, element) => {
+      const {
+        key, value, status, children,
+      } = element;
+      if (status === 'added') {
+        return `${acc}\n${leftShift}+ ${key}:${makeDataAsString(value, currentDepth + 1)}`;
+      }
+      if (status === 'deleted') {
+        return `${acc}\n${leftShift}- ${key}:${makeDataAsString(value, currentDepth + 1)}`;
+      }
+      if (status === 'unmodified') {
+        return `${acc}\n${leftShift}  ${key}:${makeDataAsString(value, 1)}`;
+      }
+      if (status === 'nested') {
+        return `${acc}\n${leftShift}  ${key}: {${iter(children, currentDepth + 1)}\n${bracketInd}}`;
+      }
+      return `${acc}${iter(element, currentDepth)}`;
+    }, '');
+    return `${formattedResult}`;
+  };
+  return `{${(iter(structureOfDiff, 1))}\n}`;
 };
 
 const genDiff = (path1, path2) => {
   const contentOfFile1 = getContentAsObject(path1);
   const contentOfFile2 = getContentAsObject(path2);
-  const combinedObj = { ...contentOfFile1, ...contentOfFile2 };
-  const keys = _.orderBy(Object.keys(combinedObj));
-  const result = keys.reduce((acc, key) => `${acc}\n ${compareProperties(contentOfFile1, contentOfFile2, key)}`, '');
-  return `{${result}\n}`;
+  return stylish(makeDiffStructure(contentOfFile1, contentOfFile2));
 };
+
+console.log(genDiff('__fixtures__/before.json', '__fixtures__/after.json'));
 
 export default genDiff;
