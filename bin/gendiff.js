8f7c1439882faf129853a3ce5d4b6b9866648acd
Olga Efremova <pcholyk@yandex.ru>
2023-07-13 11:11:22 +0300

    add option to choose format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,9 @@ program
   .argument('<filepath1>')
   .argument('<filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.format)))
+  .action((filepath1, filepath2, option) => {
+    console.log(genDiff(filepath1, filepath2, option.format));
+  })
   .helpOption('-h, --help', 'output usage information');
 
 program.parse();
