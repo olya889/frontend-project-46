95dc1f71966a3fce7564b3ea04baf6a92a5beeb6
Olga Efremova <pcholyk@yandex.ru>
2023-07-17 22:03:05 +0300

    add json function as option in getFormatterName function

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,13 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const getFormatterName = (formatName) => {
   if (formatName === 'plain') {
     return plain;
+  }
+  if (formatName === 'json') {
+    return json;
   }
   return stylish;
 };
