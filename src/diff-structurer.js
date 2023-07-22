03f583bc3d1e0e32a994feef66d687a8943fdfe0
Olga Efremova <pcholyk@yandex.ru>
2023-07-22 23:02:25 +0300

    replace make makeDiffStructure function in separate module

---
+++ src/diff-structurer.js
@@ -0,0 +1,36 @@
+import _ from 'lodash';
+
+const makeDiffStructure = (objectBefore, objectAfter) => {
+  const combinedObject = { ...objectBefore, ...objectAfter };
+  const keys = _.orderBy(Object.keys(combinedObject));
+  const diffStructure = keys.map((key) => {
+    if (!Object.hasOwn(objectBefore, key)) {
+      return {
+        key, value: objectAfter[key], status: 'added', children: [],
+      };
+    }
+    if (!Object.hasOwn(objectAfter, key)) {
+      return {
+        key, value: objectBefore[key], status: 'deleted', children: [],
+      };
+    }
+    if (objectBefore[key] === objectAfter[key]) {
+      return {
+        key, value: objectBefore[key], status: 'unmodified', children: [],
+      };
+    }
+    if (typeof objectBefore[key] === 'object' && objectBefore[key] !== null && typeof objectAfter[key] === 'object' && objectAfter[key] !== null) {
+      return { key, status: 'nested', children: makeDiffStructure(objectBefore[key], objectAfter[key]) };
+    }
+    return [{
+      key, value: objectBefore[key], status: 'deleted', children: [],
+    },
+    {
+      key, value: objectAfter[key], status: 'added', children: [],
+    },
+    ];
+  });
+  return diffStructure;
+};
+
+export default makeDiffStructure;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import fs from 'fs';
 import { cwd } from 'node:process';
 import path from 'path';
-import _ from 'lodash';
+// import _ from 'lodash';
 import parse from './parser.js';
 import stylish from './formatters/stylish.js';
 import getFormatterName from './formatters/index.js';
+import makeDiffStructure from './diff-structurer.js';
 
 const getAbsolutePath = (givenPath) => path.resolve(cwd(), givenPath);
 const getExtension = (filePath) => path.extname(filePath.toString()).toLowerCase();
@@ -15,39 +16,6 @@ const getContentAsObject = (filePath) => {
   const extension = getExtension(absolutePath);
   const content = getContent(absolutePath);
   return parse(content, extension);
-};
-
-const makeDiffStructure = (objectBefore, objectAfter) => {
-  const combinedObject = { ...objectBefore, ...objectAfter };
-  const keys = _.orderBy(Object.keys(combinedObject));
-  const diffStructure = keys.map((key) => {
-    if (!Object.hasOwn(objectBefore, key)) {
-      return {
-        key, value: objectAfter[key], status: 'added', children: [],
-      };
-    }
-    if (!Object.hasOwn(objectAfter, key)) {
-      return {
-        key, value: objectBefore[key], status: 'deleted', children: [],
-      };
-    }
-    if (objectBefore[key] === objectAfter[key]) {
-      return {
-        key, value: objectBefore[key], status: 'unmodified', children: [],
-      };
-    }
-    if (typeof objectBefore[key] === 'object' && objectBefore[key] !== null && typeof objectAfter[key] === 'object' && objectAfter[key] !== null) {
-      return { key, status: 'nested', children: makeDiffStructure(objectBefore[key], objectAfter[key]) };
-    }
-    return [{
-      key, value: objectBefore[key], status: 'deleted', children: [],
-    },
-    {
-      key, value: objectAfter[key], status: 'added', children: [],
-    },
-    ];
-  });
-  return diffStructure;
 };
 
 const genDiff = (path1, path2, formatName = stylish) => {
