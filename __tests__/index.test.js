c525de1aa537e8c178afef0c527ed5ebe7a56642
Olga Efremova <pcholyk@yandex.ru>
2023-07-30 07:51:28 +0300

    delete comments

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,8 +3,6 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
-// import expectedResult from '../__fixtures__/expected_result.js';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
