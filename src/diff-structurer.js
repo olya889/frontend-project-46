15bd21a9f74d5110744123063f277d358cf39eff
Olga Efremova <pcholyk@yandex.ru>
2023-10-31 22:30:53 +0300

    replace map with forEach for avoiding too many returns

--- src/diff-structurer.js
+++ src/diff-structurer.js
@@ -3,29 +3,28 @@ import _ from 'lodash';
 const makeDiffStructure = (objectBefore, objectAfter) => {
   const combinedObject = { ...objectBefore, ...objectAfter };
   const keys = _.orderBy(Object.keys(combinedObject));
-  const diffStructure = keys.map((key) => {
+  const diffStructure = [];
+  keys.forEach((key) => {
     if (!Object.hasOwn(objectBefore, key)) {
-      return {
+      diffStructure.push({
         key, value: objectAfter[key], status: 'added',
-      };
-    }
-    if (!Object.hasOwn(objectAfter, key)) {
-      return {
+      });
+    } else if (!Object.hasOwn(objectAfter, key)) {
+      diffStructure.push({
         key, value: objectBefore[key], status: 'deleted',
-      };
-    }
-    if (objectBefore[key] === objectAfter[key]) {
-      return {
+      });
+    } else if (objectBefore[key] === objectAfter[key]) {
+      diffStructure.push({
         key, value: objectBefore[key], status: 'unmodified',
-      };
-    }
-    if (_.isObject(objectBefore[key]) && objectBefore[key] !== null
+      });
+    } else if (_.isObject(objectBefore[key]) && objectBefore[key] !== null
         && _.isObject(objectAfter[key]) && objectAfter[key] !== null) {
-      return { key, status: 'nested', children: makeDiffStructure(objectBefore[key], objectAfter[key]) };
-    }
-    return {
+          diffStructure.push({ key, status: 'nested', children: makeDiffStructure(objectBefore[key], objectAfter[key]) });
+    } else {
+    diffStructure.push({
       key, previousValue: objectBefore[key], value: objectAfter[key], status: 'modified',
-    };
+    });
+    }
   });
   return diffStructure;
 };
